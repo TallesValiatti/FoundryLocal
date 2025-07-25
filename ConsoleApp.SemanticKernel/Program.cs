452ce5789f1bf49f732fef47cef1157e32eee09d
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2025-07-24 23:29:42 -0300

    first commit

---
+++ ConsoleApp.SemanticKernel/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.AddOpenAIChatCompletion(
+    "Phi-3-mini-4k-instruct-generic-cpu",
+    new Uri("http://localhost:5273/v1"), 
+    "");
+
+Kernel kernel = kernelBuilder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = [];
+history.AddUserMessage("What's the capital of Brazil?");
+
+
+var response = await chatCompletionService.GetChatMessageContentAsync(
+    history,
+    kernel: kernel
+);
+
+Console.WriteLine(response);
